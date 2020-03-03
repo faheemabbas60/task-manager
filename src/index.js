639d60603205e07ec47af44a1b3e98f92b97904e
Faheem Abbas <faheem.abbas@skyelectric.com>
2020-03-03 12:38:16 +0500

    tasks assigment

--- src/index.js
+++ src/index.js
@@ -17,15 +17,59 @@ app.post('/users', (req, res) => {
     })
 
 })
-app.get('/users',((req,res)=>{
-    User.find({}).then((users)=>{
+app.get('/users', ((req, res) => {
+    User.find({}).then((users) => {
         res.status(201).send(users)
         console.log('users', users)
-    }).catch((e)=>{
+    }).catch((e) => {
         res.status(400).send("Error while fetching" + e)
         console.log('failed', e)
     })
 }))
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    console.log(req.params)
+    User.findById(_id).then((user) => {
+        if (!user) {
+            res.status(404).send()
+        }
+        else {
+            res.status(201).send(user)
+            console.log('user', user)
+        }
+
+    }).catch((e) => {
+        res.status(400).send("Error while fetching" + e)
+        console.log('failed', e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        if (!tasks) {
+            res.status(404).send()
+        }
+        else {
+            res.status(201).send(tasks)
+        }
+    }).catch((e) => {
+        res.status(400).send("Error while fetching" + e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    console.log(req.params)
+    Task.findById(req.params.id).then((task) => {
+        if (!task) {
+            res.status(404).send()
+        }
+        else {
+            res.status(201).send(task)
+        }
+    }).catch((e) => {
+        res.status(400).send("Error while fetching" + e)
+    })
+})
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
